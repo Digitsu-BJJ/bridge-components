f0f55fd1a6efa96d20d83cbad6346e56c6220c4f
Shawn Aukstakalnis <salvitin@gmail.com>
2025-10-18 22:04:01 -0400

    Update hotwire-native-ios dependency version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/hotwired/hotwire-native-ios.git",
-            .upToNextMinor(from: "1.2.0")
+            .upToNextMinor(from: "1.3.0-beta")
         )
     ],
     targets: [
